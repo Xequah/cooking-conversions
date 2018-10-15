8d56aeda9bc01b873823885657d90fc0a05f6bb3
charles robertson and stuart gill <stuart@>
2018-10-15 14:23:29 -0700

    Initializes html and directories

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var userGallon = prompt ("How many gallons would you like to convert?");
+
+var userGallon = parseInt(userGallon);
+var liters = userGallon/3.78541;
+alert (userGallon + " gallons equals " + liters + "liters");
